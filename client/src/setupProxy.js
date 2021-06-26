23e04332a7dfe137adadc7b5ad257e4d573acd32
kinshukshah <kinshukshah@gmail.com>
2021-06-26 22:15:59 +0530

    Updates

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,12 +1,27 @@
+const express = require("express");
 const { createProxyMiddleware } = require("http-proxy-middleware");
+//const app = express();
 
 module.exports = function (app) {
-  app.use(
-    "/api",
-    createProxyMiddleware({
-      // target: "http://localhost:5000",
-      target: "https://play-videolibrary.herokuapp.com",
-      changeOrigin: true,
-    })
-  );
+  if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
+    // dev code
+    app.use(
+      "/api",
+      createProxyMiddleware({
+        target: "http://localhost:5000",
+        //target: "https://play-videolibrary.herokuapp.com",
+        changeOrigin: true,
+      })
+    );
+  } else {
+    // production code
+    app.use(
+      "/api",
+      createProxyMiddleware({
+        //target: "http://localhost:5000",
+        target: "https://play-videolibrary.herokuapp.com",
+        changeOrigin: true,
+      })
+    );
+  }
 };
